313f4eafd548560bef9b288e70a35e7a45ac7a5f
Shukra Tamang <shukratamang.npl@gmail.com>
2024-12-27 21:49:59 +1100

    Product page

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { Label } from "@radix-ui/react-label";
+import { useState } from "react";
 
 export function ProductForm() {
+  const [priceInCents, setPriceInCents] = useState<number>();
   return (
     <form className="space-y-8">
       <div className="space-y-2">
@@ -11,7 +13,15 @@ export function ProductForm() {
       </div>
       <div className="space-y-2">
         <Label htmlFor="name">Price in Cents</Label>
-        <input type="text" id="priceInCents" name="priceInCents" required />
+        <input
+          type="number"
+          id="priceInCents"
+          name="priceInCents"
+          required
+          value={priceInCents}
+          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
+        />
+        <div></div>
       </div>
     </form>
   );
