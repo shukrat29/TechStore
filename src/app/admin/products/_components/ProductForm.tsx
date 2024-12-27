4a728b26d0ef6b6b5b16e246e69ca6d50d541762
Shukra Tamang <shukratamang.npl@gmail.com>
2024-12-27 21:16:12 +1100

    Product form update

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,80 +1,18 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import { formatCurrency } from "@/lib/formatters";
-import { useState } from "react";
-import { addProduct } from "../../_actions/products";
-import { useFormState, useFormStatus } from "react-dom";
+import { Label } from "@radix-ui/react-label";
 
-export const ProductForm = () => {
-  const [error, action] = useFormState(addProduct, {});
-  const [priceInCents, setPriceInCents] = useState<number>();
+export function ProductForm() {
   return (
-    <form action={action} className="space-y-8">
+    <form className="space-y-8">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required />
-        {error.name && <div className="text-destructive">{error.name}</div>}
+        <input type="text" id="name" name="name" required />
       </div>
       <div className="space-y-2">
-        <Label htmlFor="priceInCents">Price In Cents</Label>
-        <Input
-          type="number"
-          id="priceInCents"
-          name="priceInCents"
-          required
-          value={priceInCents}
-          onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
-        />
-        <div className="text-muted-foreground">
-          {formatCurrency((priceInCents || 0) / 100)}
-        </div>
-        {error.priceInCents && (
-          <div className="text-destructive">{error.priceInCents}</div>
-        )}
+        <Label htmlFor="name">Price in Cents</Label>
+        <input type="text" id="priceInCents" name="priceInCents" required />
       </div>
-      <div className="space-y-2">
-        <Label htmlFor="description">Description</Label>
-        <Textarea id="description" name="description" required />
-        {error.description && (
-          <div className="text-destructive">{error.description}</div>
-        )}
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="file">File</Label>
-        <Input
-          type="file"
-          id="file"
-          name="file"
-          required
-          className="cursor-pointer"
-        />
-        {error.file && <div className="text-destructive">{error.file}</div>}
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="file">Image</Label>
-        <Input
-          type="file"
-          id="image"
-          name="image"
-          required
-          className="cursor-pointer"
-        />
-        {error.image && <div className="text-destructive">{error.image}</div>}
-      </div>
-      <SubmitButton />
     </form>
-  );
-};
-
-function SubmitButton() {
-  const { pending } = useFormStatus();
-  return (
-    <Button type="submit" disabled={pending}>
-      {pending ? "Saving..." : "Save"}
-    </Button>
   );
 }
